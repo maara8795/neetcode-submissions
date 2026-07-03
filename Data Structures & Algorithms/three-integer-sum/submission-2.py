28f078c9dada5d1c1e55155ba51595278c8e4930
maara8795 <maara8795@gmail.com>
2026-07-03 14:57:14 -0700

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        #Brute force is O(n cube) and O(m = n square worset care)
+        #Hash Map O(n square) and O(n)
+        #Two pointers O(n square) and O(1)
+
+        nums.sort()
+        res = []
+        
+        for i in range(len(nums)):
+
+            if nums[i] > 0 :
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            j = i + 1
+            k = len(nums)-1
+            while j<k:
+                if nums[i]+nums[j]+nums[k] > 0:
+                    k-=1
+                elif nums[i]+nums[j]+nums[k] < 0:
+                    j+=1
+                else:
+                    res.append([nums[i],nums[j],nums[k]])
+                    j+=1
+                    k-=1
+                    while j<k and nums[j] == nums[j-1]:
+                        j+=1
+
+        return res
+
+
